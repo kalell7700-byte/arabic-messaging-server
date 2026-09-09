7af27e4c965c3f3ecd156a7b62b9389863309a17
kalell7700-byte <kalell7700@gmail.com>
2026-09-10 02:23:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -102,6 +102,6 @@ io.on('connection', socket => {
     } catch { cb({ ok:false, error:'تعذر إرسال الرسالة' }); }
   });
   socket.on('disconnect', () => { if (online.get(socket.user.id) === socket.id) online.delete(socket.user.id); });
-}):
+});
 
 server.listen(PORT, '0.0.0.0', () => console.log(`Messaging server running on port ${PORT}`));
