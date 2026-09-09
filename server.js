13127f2b5f2b8ad7890f56da7426a0542cfaa1f0
kalell7700-byte <kalell7700@gmail.com>
2026-09-10 02:35:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -105,6 +105,5 @@ io.on('connection', socket => {
 });
 
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Messaging server running on port ${PORT}`));
 
+server.listen(PORT, '0.0.0.0', () => console.log(`Messaging server running on port ${PORT}`));
