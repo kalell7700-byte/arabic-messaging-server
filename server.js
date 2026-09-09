2085f9051332e10ce0e8c060d96ca4aaf9d87ee0
kalell7700-byte <kalell7700@gmail.com>
2026-09-10 02:26:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -104,4 +104,6 @@ io.on('connection', socket => {
   socket.on('disconnect', () => { if (online.get(socket.user.id) === socket.id) online.delete(socket.user.id); });
 });
 
+
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, '0.0.0.0', () => console.log(`Messaging server running on port ${PORT}`));
